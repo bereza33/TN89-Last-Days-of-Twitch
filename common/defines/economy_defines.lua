0ae56eb0f3acf21adfc067d8abfb049261f26979
Vladyslav <berezin.vlad10@gmail.com>
2026-06-10 20:26:18 +0200

    initial

---
+++ common/defines/economy_defines.lua
@@ -0,0 +1,6 @@
+NDefines.NProduction.RESOURCE_TO_ENERGY_COEFFICIENT = 10.0		-- How much energy per coal produces
+NDefines.NProduction.BASE_COUNTRY_ENERGY_PRODUCTION = 10.0 			-- The base energy production of a country
+NDefines.NProduction.ENERGY_SCALING_COST_BY_FACTORY_COUNT = 0.025 -- Scales energy cost based on the total number of factories
+NDefines.NProduction.BASE_ENERGY_COST = 0.25						-- How much energy per factory consumes
+NDefines.NProduction.ENERGY_COST_CAP = 10						-- Maximum energy cost per factory
+NDefines.NProduction.ENERGY_SCALE_PER_TRADE_FACTORY_EXPORT = 0.25 -- Factor of how many of the factories gained from trade is affects the energy cost scaling
